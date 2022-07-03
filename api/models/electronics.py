b97423eab5b4a545310be41f21f612811afbd813
Imran Syed <imran@Imrans-MacBook-Air.local>
2022-07-03 06:16:11 -0500

    added new field for images

--- api/models/electronics.py
+++ api/models/electronics.py
@@ -4,6 +4,6 @@ class Electronic(models.Model):
     name = models.CharField(max_length=100)
     price = models.CharField(max_length=100)
     rating = models.CharField(max_length=100)
-    image_url = models.CharField(max_length=100)
+    image_url = models.CharField(max_length=1000)
 
 
