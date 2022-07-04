c348e1e7da056bb0cb33b11059f21907ce9d4862
Imran Syed <imran@Imrans-MacBook-Air.local>
2022-07-03 23:55:54 -0500

    changed charfield to integerfield

--- api/models/electronics.py
+++ api/models/electronics.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Electronic(models.Model):
     name = models.CharField(max_length=100)
-    price = models.CharField(max_length=100)
+    price = models.IntegerField()
     rating = models.CharField(max_length=100)
     image_url = models.CharField(max_length=1000)
 
